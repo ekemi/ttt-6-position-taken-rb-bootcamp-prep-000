f42274ff38aeeb6f9c4114b7806139007dcaa8b3
edmundo papi <edmundopierre@yahoo.com>
2020-03-07 22:00:44 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -10,5 +10,10 @@ def position_taken?(board,index)
 
     elsif board[index] == nil
        return false
+
+    elsif board[index] == "X" || board[index]=="O"
+
+      return true
+         
     end
 end
